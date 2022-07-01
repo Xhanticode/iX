70ca7c914691fbc3a4485b861553c51d2f244701
Xhanti <xhantimpu@gmail.com>
2022-07-01 17:28:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const featuredProducts = [
+    {Title: "MacBook Air",
+        Year: 2019,
+    Colour: "Gold",
+    Price: "R16 499.99",
+Cat: MacBooks,
+Img:,}
+    {Title: "iPhone 12 Pro Max",
+        Year: 2021,
+    Colour: "Gold",
+    Price: "R14 999.99",
+Cat: iPhones,
+Img:,}
+    {Title: "AirPods 2",
+        Year: 2020,
+    Colour: "White",
+    Price: "R2 399.99",
+Cat: Airpods,
+Img:,}
+    {Title: "Apple Watch 4",
+        Year: 2019,
+    Colour: "Black",
+    Price: "R4 999.99",
+Cat: "iWatch",
+Img:,},
+    {Title: "iPhone 13 mini",
+        Year: 2021,
+    Colour: "Space Grey",
+    Price: "13 499.99",
+Cat: "iPhone",
+Img:,},
+    {Title: `MacBook Pro 16"`,
+        Year: 2019,
+    Colour: "Silver",
+    Price: "R26 999.99",
+Cat: MacBooks,
+Img:,}
+]
